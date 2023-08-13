9903344324e5bbb3d18715b87b613ca34e195c24
Pape Barka <papebarka@gmail.com>
2023-08-13 23:47:03 +0000

    Corrects syntax error in store

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,9 @@ import { createStore } from 'vuex'
 import { albums } from './albums'
 import { photos } from './photos'
 
-export const store = createStore{
+export const store = createStore({
     modules: {
         albums,
         photos
     }
-}
+})
